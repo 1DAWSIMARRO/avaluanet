f97ff4a3bf579444cf897ca78dee82661a132ad4
SaMuEl13GiNeR <sam.gi2002@gmail.com>
2021-06-02 12:33:53 +0200

    Update ProfessorModel.php

--- modules/Professor/model/ProfessorModel.php
+++ modules/Professor/model/ProfessorModel.php
@@ -1,11 +1,29 @@
 <?php
-class ProfessorModel{
-    function __construct(){
-        $this->DB=Database::connect();
+class ProfessorModel
+{
+    function __construct()
+    {
+        $this->DB = Database::connect();
     }
-    function registrarM(){
+    function registrarM($data)
+    {
         $sql = "INSERT INTO professor (dni,nom,cognoms,login,password,email) VALUES (?,?,?,?,?,?)";
-        $stmt=$this->DB->prepare($sql);
-        $stmt->execute(["012345","Samu","Giner","123","123","123@gmail.com"]);
+        $stmt = $this->DB->prepare($sql);
+        $stmt->execute([$data['dni'], $data['nom'], $data['cognoms'], $data['login'], $data['password'], $data['email']]);
+    }
+
+    function accederM($data)
+    {
+        $sql = "SELECT professor.login, professor.password from professor where professor.login LIKE " . $data['login'] . " AND professor.password LIKE " . $data['password'];
+        $result = mysqli_query(mysqli_connect("localhost", "userava", "userava", "avaluanet"), $sql);
+        if ($result) {
+            $row = mysqli_num_rows($result);
+            if ($row) {
+                printf("Usuario con Login: " . $data['login'] . " Password: " . $data['password']);
+            }
+        } else {
+            echo "El usuario introducido no existe. Registrate antes de acceder.";
+            header("location: http://localhost/AVALUANET/modules/Professor/view/registrar.html");
+        }
     }
 }

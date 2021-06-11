07763552fbcbb2da85e277a0cf257b7cc207cde1
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-11 10:31:19 +0200

    Update GrupModel.php

--- Modules/Grup/model/GrupModel.php
+++ Modules/Grup/model/GrupModel.php
@@ -1,5 +1,5 @@
 <?php
-require_once('./connexio.php');
+// require_once('../Grup/connexio.php');
     class GrupModel {
 
     function __construct(){
@@ -9,31 +9,38 @@ require_once('./connexio.php');
 
     function altaM($data){
 
-        $sql = "INSERT INTO grup (nom, curs, aula, n_alumnes), VALUES(?,?,?,?)";
-        $this->DB->prepare($sql);
-        $this->DB->exec($data['nom'], $data['curs'], $data['aula'], $data['n_alumnes']);
+        $sql = "INSERT INTO grup (nom, curs, aula, n_alumnes) VALUES (?,?,?,?)";
+        $stmt=$this->DB->prepare($sql);
+        $stmt->execute([$data['nom'], $data['curs'], $data['aula'], $data['n_alumnes']]);
+    }
+
+    function obtindreGrupM($codi){
+        
+        $sql = "SELECT * FROM grup where codi = ?";
+        $q = $this->DB->prepare($sql);
+        $q->execute(array($codi));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        return $data;
     }
 
     function baixaM($data){
 
-        $sql = "DELETE FROM grup WHERE codi = $data";
-        $this->DB->exec($sql);       
+        $sql = "DELETE FROM grup WHERE codi = ?";
+        $stmt=$this->DB->prepare($sql);
+        $stmt->execute([$data['codi']]);  
     }
 
-    function modificacio($data){
-        $sql = "UPDATE grup SET nom = ?, curs = ?, aula = ?, n_alumnes = ? WHERE codi = $data";
-        $this->DB->exec($sql);
+    function modificacioM($data){
         
+        $sql = "UPDATE grup SET nom = ?, curs = ?, aula = ?, n_alumnes = ? WHERE codi = ?";
+        $stmt=$this->DB->prepare($sql);
+        $stmt->execute([$data['nom'], $data['curs'], $data['aula'], $data['n_alumnes'], $data['codi']]);  
     }
 
     function llistarM(){
 
         $sql="SELECT * FROM grup";
-        print_r($this->DB->query($sql)->fetchAll(PDO::FETCH_ASSOC));
-        // return $this->DB->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        return $this->DB->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
   }
-  $model = new GrupModel();
-  $model->llistarM();
-  $model->altaM('1DAW', 'GS', 20, 14);
 ?>

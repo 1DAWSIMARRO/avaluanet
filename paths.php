ac985968ac2dd5fdd459ad73e7a49d97aca0db57
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-11 10:29:58 +0200

    Create paths.php

---
+++ paths.php
@@ -0,0 +1,23 @@
+<?php
+
+define('VIEW_D','utils/view/');
+
+//PATTERN
+//COM FUNCIONA -> pattern(nom_modul_majuscules,nom_modul_primera_lletra_majuscula);
+//RUTES AL CONTROLLER -> CONTROLLER_NOMMODULMAJUSCULA
+//igual per al model i vista
+
+function pattern ($module_u, $module){
+    define('CONTROLLER_' . $module_u,'modules/'.$module.'/controller/');
+    define('MODEL_' . $module_u,'modules/'.$module.'/model/');
+    define('VIEW_' . $module_u,'modules/'.$module.'/view/');
+}
+
+//ALUMNE MODULE
+pattern('ALUMNE','Alumne');
+pattern('PROFESSOR','Professor');
+pattern('ASIGNATURA','Asignatura');
+pattern('GRUP','Grup');
+
+
+?>

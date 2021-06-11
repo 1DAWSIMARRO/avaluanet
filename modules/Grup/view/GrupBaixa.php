62114b7befe2176644caeba409d3cf48539e2e6d
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-11 10:31:29 +0200

    Create GrupBaixa.php

---
+++ modules/Grup/view/GrupBaixa.php
@@ -0,0 +1,5 @@
+<form action="index.php" method="REQUEST">
+    <input type="hidden" name="metode" value="baixa">
+    <label for="codi">Codi</label>
+    <input type="text" name="codi" value="<?php echo $data['codi']?>"><input type="submit" value="Send" id="send">
+</form>

414fc599a7ecc6f293a5f094e1891232a761e786
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-11 10:31:16 +0200

    Update GrupController.php

--- Modules/Grup/controller/GrupController.php
+++ Modules/Grup/controller/GrupController.php
@@ -1,26 +1,76 @@
 <?php
-  require_once('./model/GrupModel.php');
+  require_once(MODEL_GRUP.'GrupModel.php');
   class GrupController {
 
     function __construct(){
-
+      
       $this->Model = new GrupModel();
+    }
+
+    public function llistar() {
+
+      $list = $this->Model->llistarM(); 
+      include_once(VIEW_GRUP.'GrupLlistar.php');
+    }
+
+    function alta() {
+        // FORMA DE CONSEGUIR DATOS
         
+      if (isset($_REQUEST['nom'])) {
+        
+        $data=[];
+        foreach ($_REQUEST as $key => $value) {
+
+          $data[$key] = $value;
+        }
+        $this->Model->altaM($data);
+        header('Location: index.php');  // VUELVE A MOSTRAR EL PHP index.php
+
+      } else {
+
+        include_once(VIEW_GRUP."GrupAlta.php");
+      }
+
     }
 
-    function alta()
-    {
-      # code...
+    function baixa() {
+      
+      if (isset($_REQUEST['codi'])) {
+        
+        $data=[];
+        foreach ($_REQUEST as $key => $value) {
+          
+          $data[$key] = $value;
+        }
+        $this->Model->baixaM($data);
+        header('Location: index.php');
+
+      } else {
+
+        include_once(VIEW_GRUP."GrupBaixa.php");
+      }
     }
 
-    function baixa()
-    {
-      # code...
+    function modificacio() {
+
+      
+      if (isset($_REQUEST['codi'])) {
+        
+        $data=[];
+        foreach ($_REQUEST as $key => $value) {
+          
+          $data[$key] = $value;
+        }
+        $this->Model->modificacioM($data);
+        header('Location: index.php');
+      }
+      include_once(VIEW_GRUP."GrupModificar.php");
     }
 
-    function modificacio()
-    {
-      # code...
+    function viewEditar() {
+
+        $data = $this->Model->obtindreGrupM($_REQUEST['codi']); 
+        include_once(VIEW_GRUP."GrupModificar.php");
     }
   }
 ?>

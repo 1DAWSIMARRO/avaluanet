87a292c8380d8964455c240a715fdba66d22faf5
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-11 10:31:32 +0200

    Create GrupModificar.php

---
+++ modules/Grup/view/GrupModificar.php
@@ -0,0 +1,45 @@
+<form action="index.php" method="REQUEST">
+    <input type="hidden" name="metode" value="modificacio">
+    <label for="codi">Codi</label>
+    <input type="text" name="codi" value="
+        <?php 
+            if (isset($data)) {
+                echo $data['codi'];
+            }
+        ?>
+    ">        
+    <label for="nom">Nom</label>
+    <input type="text" name="nom" value="
+        <?php
+            if (isset($data)) {
+                echo $data['nom'];
+            }
+        ?>
+    ">
+    <label for="curs">Curs</label>
+    <input type="text" name="curs" value="
+        <?php
+            if (isset($data)) {
+                echo $data['curs'];
+            }
+        ?>
+    ">
+    <label for="aula">Aula</label>
+    <input type="text" name="aula" value="
+        <?php
+            if (isset($data)) {
+                echo $data['aula'];
+            }
+        ?>
+    ">
+    <label for="n_alumnes">N_alumnes</label>
+    <input type="text" name="n_alumnes" value="
+        <?php
+            if (isset($data)) {
+                echo $data['n_alumnes'];
+            }
+            $url = "index.php?module=Grup&function=modificacio&codi=".$data['codi'];
+        ?>
+    ">
+    <a href="<?php echo $url ?>"><input type="button" value="MODIFICAR"></a>
+</form>

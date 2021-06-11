9b981935213a724be9cb82be2f40c2a1f35a05ed
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-11 10:31:27 +0200

    Create GrupAlta.php

---
+++ modules/Grup/view/GrupAlta.php
@@ -0,0 +1,12 @@
+<form action="index.php" method="REQUEST">
+    <input type="hidden" name="metode" value="modificacio">
+    <label for="nom">Nom</label>
+    <input type="text" name="nom">
+    <label for="curs">Curs</label>
+    <input type="text" name="curs">
+    <label for="aula">Aula</label>
+    <input type="text" name="aula">
+    <label for="n_alumnes">N_alumnes</label>
+    <input type="text" name="n_alumnes">
+    <input type="submit" value="Send" id="send">
+</form>

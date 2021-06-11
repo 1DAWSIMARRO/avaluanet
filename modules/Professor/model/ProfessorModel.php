666c53948f2ad437ac82982e0364f80442c3e2de
SaMuEl13GiNeR <sam.gi2002@gmail.com>
2021-06-11 12:19:23 +0200

    Update ProfessorModel.php

--- modules/Professor/model/ProfessorModel.php
+++ modules/Professor/model/ProfessorModel.php
@@ -7,17 +7,20 @@ class ProfessorModel
     }
     function registrarM($data)
     {
-        $sql1 = "SELECT professor.dni from professor where professor.dni LIKE " . $data['dni'];
+        $sql1 = "SELECT professor.dni from professor where professor.dni LIKE '" . $data['dni']."'";
+        $sql2 = "SELECT professor.login from professor where professor.login LIKE '" . $data['login']."'";
         $result = mysqli_query(mysqli_connect("localhost", "userava", "userava", "avaluanet"), $sql1);
-        if ($result) {
+        $result2 = mysqli_query(mysqli_connect("localhost", "userava", "userava", "avaluanet"), $sql2);
+        if ($result && $result2) {
             $row = mysqli_num_rows($result);
-            if ($row == 0) {
+            $row2 = mysqli_num_rows($result2);
+            if ($row == 0 && $row2 == 0) {
                 $sql = "INSERT INTO professor (dni,nom,cognoms,login,password,email) VALUES (?,?,?,?,?,?)";
                 $stmt = $this->DB->prepare($sql);
                 $stmt->execute([$data['dni'], $data['nom'], $data['cognoms'], $data['login'], $data['password'], $data['email']]);
-                return "false";
+                return "noexiste";
             } else {
-                return "true";
+                return "existe";
             }
         } else {
             return "ERROR mysqli_query";

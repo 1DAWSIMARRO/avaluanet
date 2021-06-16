c702f7cc33dfa555cd3d2741184608c947e73538
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-16 08:34:14 +0200

    Update GrupAlta.php

--- modules/Grup/view/GrupAlta.php
+++ modules/Grup/view/GrupAlta.php
@@ -1,12 +1,37 @@
 <form action="index.php" method="REQUEST">
-    <input type="hidden" name="metode" value="modificacio">
+    <input type="hidden" name="metode" value="alta">
     <label for="nom">Nom</label>
-    <input type="text" name="nom">
+    <input type="text" name="nom" value="
+        <?php
+            if (isset($data)) {
+                echo $data['nom'];
+            }
+        ?>
+    ">
     <label for="curs">Curs</label>
-    <input type="text" name="curs">
+    <input type="text" name="curs" value="
+        <?php
+            if (isset($data)) {
+                echo $data['curs'];
+            }
+        ?>
+    ">
     <label for="aula">Aula</label>
-    <input type="text" name="aula">
+    <input type="text" name="aula" value="
+        <?php
+            if (isset($data)) {
+                echo $data['aula'];
+            }
+        ?>
+    ">
     <label for="n_alumnes">N_alumnes</label>
-    <input type="text" name="n_alumnes">
-    <input type="submit" value="Send" id="send">
+    <input type="text" name="n_alumnes" value="
+        <?php
+            if (isset($data)) {
+                echo $data['n_alumnes'];
+            }
+            $url = "index.php?module=Grup&function=alta";
+        ?>
+    ">
+    <a href="<?php echo $url ?>"><input type="button" value="ALTA"></a>
 </form>

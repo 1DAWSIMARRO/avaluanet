d42f9169b5f9afc7dd06667e801a7c2dc37a1b19
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-16 08:38:47 +0200

    Update GrupModificar.php

--- modules/Grup/view/GrupModificar.php
+++ modules/Grup/view/GrupModificar.php
@@ -5,44 +5,34 @@ $url = "index.php?module=Grup&function=modificacio&codi=".$data['codi'];
 <form action="<?php echo $url ?>" method="POST">
     <input type="hidden" name="metode" value="modificacio">
     <label for="codi">Codi</label>
-    <input type="text" name="codi" value="
-        <?php 
+    <input type="text" name="codi" value="<?php 
             if (isset($data)) {
                 echo $data['codi'];
             }
-        ?>
-    ">        
+        ?>">        
     <label for="nom">Nom</label>
-    <input type="text" name="nom" value="
-        <?php
+    <input type="text" name="nom" value="<?php
             if (isset($data)) {
                 echo $data['nom'];
             }
-        ?>
-    ">
+        ?>">
     <label for="curs">Curs</label>
-    <input type="text" name="curs" value="
-        <?php
+    <input type="text" name="curs" value="<?php
             if (isset($data)) {
                 echo $data['curs'];
             }
-        ?>
-    ">
+        ?>">
     <label for="aula">Aula</label>
-    <input type="text" name="aula" value="
-        <?php
+    <input type="text" name="aula" value="<?php
             if (isset($data)) {
                 echo $data['aula'];
             }
-        ?>
-    ">
+        ?>">
     <label for="n_alumnes">N_alumnes</label>
-    <input type="text" name="n_alumnes" value="
-        <?php
+    <input type="text" name="n_alumnes" value="<?php
             if (isset($data)) {
                 echo $data['n_alumnes'];
             }
-        ?>
-    ">
+        ?>">
     <a href="#"><input type="submit" value="MODIFICAR"></a>
 </form>

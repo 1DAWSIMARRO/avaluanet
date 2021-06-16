18389906714a00459ad922dda668ce2d9392027e
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-16 10:52:13 +0200

    Update AlumneAgregar.php

--- modules/Asignatura/view/AlumneAgregar.php
+++ modules/Asignatura/view/AlumneAgregar.php
@@ -5,21 +5,31 @@
     <h1>Llenguatge de marques</h1> 
   </div>
   <div class="col-1" style="margin-top: 0.5%;">
-    <button type="button" class="btn btn-success" >Editar </button>
+    <button type="button" class="btn btn-warning">Editar </button>
   </div>
   <div class="col-1" style="margin-top: 0.5%;">
-    <button type="button" class="btn btn-success" >Eliminar</button>
+    <button type="button" class="btn btn-danger">Eliminar</button>
   </div>
 </div>
 <div class="row justify-content-between">
 <div class="col-5">
-<table class="table" style="margin-top: 2%;">
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active text-dark" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Alumne</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link text-dark" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Qualificacions</button>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+  <table class="table" style="margin-top: 2%;">
   <thead class="thead-dark"> 
   <tr>
-    <th scope="col">NIA</th>
+  <th scope="col">NIA</th>
     <th scope="col">NOM</th>
     <th scope="col">COGNOMS</th>
-    <th scope="col">TELEFON</th>
+    <th scope="col">TELÈFON</th>
     <th scope="col">EMAIL</th>
     <th scope="col">codiGrup</th>
     <th></th>
@@ -39,6 +49,67 @@
   ?>
   </tbody>
 </table>
+  </div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+  <table class="table" style="margin-top: 2%;">
+  <thead class="thead-dark"> 
+  <tr>
+  <th scope="col">NIA</th>
+    <th scope="col">NOM</th>
+    <th scope="col">Avaluacio</th>
+    <th scope="col">Avaluable</th>
+    <th scope="col">Examen</th>
+    <th scope="col">Nota Final</th>
+    <th></th>
+    
+  </tr>
+  </thead>
+  <tbody>
+  <?php
+    
+    foreach ($list as $key => $value) {
+      echo '<tr>';
+      foreach ($value as $key2 => $value2) {
+        echo '<td>'.$value2.'</td>';
+      }
+      echo '<td><a class="btn btn-danger" href="index.php?module=Asignatura&function=remove&NIA='.$value['NIA'].'">Delete</a></td>';
+      echo '</tr>';
+    }
+  ?>
+  </tbody>
+</table>
+
+  </div>
+
+</div>
+
+
+<!-- <table class="table" style="margin-top: 2%;">
+  <thead class="thead-dark"> 
+  <tr>
+    <th scope="col">NIA</th>
+    <th scope="col">NOM</th>
+    <th scope="col">COGNOMS</th>
+    <th scope="col">TELEFON</th>
+    <th scope="col">EMAIL</th>
+    <th scope="col">codiGrup</th>
+    <th></th>
+  </tr>
+  </thead>
+  <tbody>
+  <?php
+    
+    // foreach ($list as $key => $value) {
+    //   echo '<tr>';
+    //   foreach ($value as $key2 => $value2) {
+    //     echo '<td>'.$value2.'</td>';
+    //   }
+    //   echo '<td><a class="btn btn-danger" href="index.php?module=Asignatura&function=remove&NIA='.$value['NIA'].'">Delete</a></td>';
+    //   echo '</tr>';
+    // }
+  ?>
+  </tbody>
+</table> -->
 </div>
 <div class="col-4">
 <table class="table" style="margin-top: 2%;">
@@ -66,6 +137,7 @@
 <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
   Agregar Alumne
 </button>
+<a class="btn btn-success">Añadir avaluable</a>
 
 <!-- Modal -->
 
@@ -86,7 +158,7 @@
     <th scope="col">NIA</th>
     <th>NOM</th>
     <th>COGNOMS</th>
-    <th>TELEFON</th>
+    <th>TELÈFON</th>
     <th>EMAIL</th>
     <th>codiGrup</th>
   </tr>

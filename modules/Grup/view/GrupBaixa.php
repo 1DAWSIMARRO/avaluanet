f7290f84c0db9f486e07b43c478b542f33aa11bb
David93-D <davidpp80.dpp@gmail.com>
2021-06-16 09:53:20 +0200

    Update GrupBaixa.php

--- modules/Grup/view/GrupBaixa.php
+++ modules/Grup/view/GrupBaixa.php
@@ -1,5 +1,14 @@
-<form action="index.php" method="REQUEST">
+<?php 
+$url = "index.php?module=Grup&function=baixa&codi=".$data['codi'];
+?>
+
+<form action="<?php echo $url ?>" method="POST">
     <input type="hidden" name="metode" value="baixa">
     <label for="codi">Codi</label>
-    <input type="text" name="codi" value="<?php echo $data['codi']?>"><input type="submit" value="Send" id="send">
+    <input type="text" name="codi" value="<?php 
+            if (isset($data)) {
+                echo $data['codi'];
+            }
+        ?>">        
+    <a href="#"><input type="submit" value="BAIXA"></a>
 </form>

9282fe1436f570f91f3ced369ab3fca950297312
David93-D <davidpp80.dpp@gmail.com>
2021-06-16 09:12:53 +0200

    Update GrupModel.php

--- modules/Grup/model/GrupModel.php
+++ modules/Grup/model/GrupModel.php
@@ -10,7 +10,6 @@
         $sql = "INSERT INTO grup (nom, curs, aula, n_alumnes) VALUES (?,?,?,?)";
         $stmt=$this->DB->prepare($sql);
         $stmt->execute([$data['nom'], $data['curs'], $data['aula'], $data['n_alumnes']]);
-        echo "Hola";
     }
 
     function obtindreGrupM($codi){

2add148b7d4b2d93374d490aad642ec65477966f
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-18 11:13:22 +0200

    Fix JS ALUMNE

--- modules/Alumne/view/comprobar.js
+++ modules/Alumne/view/comprobar.js
@@ -6,15 +6,9 @@ function validarFormulario(evento) {
     evento.preventDefault();
     var valorValido = /^[0-9]{8}$/;
     var valorTel = /^[0-9]{9}$/;
-<<<<<<< HEAD
-<<<<<<< HEAD
     var testValido =/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-=======
-    var testValido = /^[A-Z]+$/i;
->>>>>>> dev
-=======
-    var testValido = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
->>>>>>> dev
+
+
     var emailValido = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     var NIA = document.getElementById("NIA").value;
     var Nom = document.getElementById("nom").value;

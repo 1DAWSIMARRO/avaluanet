72ddd70bfc4a7a000441ca172749f31f217a48c2
IvanOviedo93 <80057746+IvanOviedo93@users.noreply.github.com>
2021-06-21 10:50:29 +0200

    remove js

--- modules/Grup/view/js/script.js
+++
@@ -1,9 +0,0 @@
-var value = null;
-
-function pasarParametro(codi) {
-    value = codi;
-}
-
-function accionEliminar() {
-    window.open("index.php?module=Asignatura&function=baixa&codi=" + value, "_self");
-}
--- modules/Grup/view/js/script_modal.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(
-    function() { $('#modalagregaralumno').modal('show'); })
